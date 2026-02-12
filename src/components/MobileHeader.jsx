c394da38a759bdede9cb111e94268b8ec29bf63f
AMIRALI <alimoradi7718@gmail.com>
2026-02-12 14:06:09 +0330

    fix something

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -6,14 +6,15 @@ export default function MobileHeader() {
       className="d-flex d-md-none sticky-top align-items-center justify-content-between w-100 px-3"
       style={{ height: "5rem", backgroundColor: "var(--golestan-dark-bg)" }}
     >
-      {/* منوی همبرگری */}
-      <button className="text-white fs-4 bg-transparent border-0 p-0">☰</button>
+      <button className="text-white fs-4 bg-transparent border-0 p-0">
+        ☰
+      </button>
 
-      {/* عنوان یا لوگو */}
       <Image src={"/logo-fr (2).svg"} width={100} height={100} alt="logo" />
 
-      {/* آیکون سبد خرید */}
-      <button className="text-white fs-4 bg-transparent border-0 p-0">🛒</button>
+      <button className="text-white fs-4 bg-transparent border-0 p-0">
+        🛒
+      </button>
     </header>
   );
 }
